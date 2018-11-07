304c66e2ea1a0852f71bfe6e2f150b67ffc3d7dc
Oliver Fleckenstein <s144472@student.dtu.dk>
2018-11-07 14:06:00 +0100

    Updates to let get_tweets run in background

--- src/get_tweets.py
+++ src/get_tweets.py
@@ -13,13 +13,20 @@ def saveTweets(airport_id: int, tweets: list):
     with open(os.path.join(tweets_directory, str(airport_id)), 'w', encoding='utf-8') as f:
         f.write(';;;\n\n'.join(tweets))
 
-if __name__ == '__main__':
-    for row in tqdm(list(airports.itertuples())):
-        airport_id = row[1]
-        name = row[2]
-        latitude = row[7]
-        longitude = row[8]
-        tweets = searchLocation(name, latitude, longitude)
-        tweets = map(lambda x: x.text, tweets)
 
-        saveTweets(airport_id, tweets)
+print('Starting retreiving tweets')
+
+for i, row in enumerate(airports.itertuples()):
+    airport_id = row[1]
+    name = row[2]
+    latitude = row[7]
+    longitude = row[8]
+    tweets = searchLocation(name, latitude, longitude)
+    tweets = map(lambda x: x.text, tweets)
+
+    saveTweets(airport_id, tweets)
+
+    if (i + 1) % 100 == 0:
+        print('{0} airports retreived'.format(i + 1))
+
+print('Finished retreiving tweets')
