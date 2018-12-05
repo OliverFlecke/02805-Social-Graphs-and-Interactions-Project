e763183c6efefc5cbfcc4328df8defa8be688a06
Oliver Fleckenstein <Oliverfl@live.dk>
2018-12-05 21:16:42 +0100

    Added normal distribtion for histogram

--- src/components/Histogram.tsx
+++ src/components/Histogram.tsx
@@ -1,4 +1,5 @@
 import * as Highcharts from 'highcharts';
+import * as _ from 'lodash';
 import React from 'react';
 import * as styles from './Histogram.module.scss';
 
@@ -10,16 +11,20 @@ interface IHistogramProps {
 
 export class Histogram extends React.Component<IHistogramProps> {
     public componentDidMount() {
+        const data = this.props.data;
+        const binData = this.binData(data);
+        const standardData = this.getNormalDistribution(data);
+
         const chart = Highcharts.chart('container', this.getOptions());
         chart.addSeries({
             name: 'Distribution',
-            data: this.binData(this.props.data),
+            data: binData,
+            type: 'column',
         });
 
         const mean =
-            this.props.data.reduce(
-                (acc: number, value: number) => acc + value,
-            ) / this.props.data.length;
+            data.reduce((acc: number, value: number) => acc + value) /
+            data.length;
 
         chart.xAxis[0].addPlotLine({
             value: mean,
@@ -30,14 +35,43 @@ export class Histogram extends React.Component<IHistogramProps> {
                 text: `Mean ${mean.toPrecision(2)}`,
             },
         });
+
+        chart.addSeries({
+            data: standardData,
+            type: 'spline',
+        });
     }
 
     public render() {
         return <div id='container' className={styles.container} />;
     }
 
+    private getNormalDistribution(data: number[]) {
+        const mean =
+            data.reduce((acc: number, value: number) => acc + value) /
+            data.length;
+        const std = Math.sqrt(
+            data
+                .map((x: number) => x - mean)
+                .map((x: number) => x ** 2)
+                .reduce((acc: number, value: number) => acc + value) /
+                (data.length - 1),
+        );
+
+        const min = data.reduce((acc, x) => (acc < x ? acc : x));
+        const max = data.reduce((acc, x) => (acc > x ? acc : x));
+        const unit = (max - min) / 100;
+
+        const normalY = (x: number, mu: number, sigma: number) =>
+            Math.exp(-0.5 * Math.pow((x - mu) / sigma, 2));
+
+        return _.range(min, max, unit).map((x: number) => ({
+            x,
+            y: normalY(x, mean, std),
+        }));
+    }
+
     private binData(data: number[]) {
-        console.debug(data[0]);
         const histData: any[] = [];
         const bins = 30;
 
@@ -79,13 +113,31 @@ export class Histogram extends React.Component<IHistogramProps> {
             }
         }
 
+        histData.forEach((bin: number[], index: number) => {
+            if (bin.length === 1) {
+                histData[index].push(0);
+            }
+        });
+
+        const maxFreq = histData.reduce(
+            (acc: number, x: number[]) => (acc > x[1] ? acc : x[1]),
+            Number.MIN_VALUE,
+        );
+        const minFreq = histData.reduce(
+            (acc: number, x: number[]) => (acc < x[1] ? acc : x[1]),
+            Number.MAX_VALUE,
+        );
+
+        histData.forEach((value: number[], index: number) => {
+            histData[index][1] = (value[1] - minFreq) / (maxFreq - minFreq);
+        });
+
         return histData;
     }
 
     private getOptions(): any {
         return {
             chart: {
-                type: 'column',
                 margin: [50, 25, 25, 50],
                 backgroundColor: 'white',
             },
@@ -95,14 +147,17 @@ export class Histogram extends React.Component<IHistogramProps> {
             legend: {
                 enabled: false,
             },
-            width: 100,
             tooltip: {},
             plotOptions: {
-                series: {
+                column: {
                     pointPadding: 0,
                     groupPadding: 0,
                     borderWidth: 0.5,
                     borderColor: 'rgba(255,255,255,0.5)',
+                    pointWidth: 15,
+                },
+                spline: {
+                    enableMouseTracking: false,
                 },
             },
             xAxis: [
