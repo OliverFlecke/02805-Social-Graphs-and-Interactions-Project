ec6713337f6feb1db1b5d918877c0a4842528b55
Oliver Fleckenstein <Oliverfl@live.dk>
2018-12-05 22:22:49 +0100

    Added slider with images

--- src/section/Sentiment.tsx
+++ src/section/Sentiment.tsx
@@ -1,8 +1,14 @@
 import Axios from 'axios';
 import React from 'react';
+import Slider from 'react-slick';
 import { Histogram } from '../components/Histogram';
 import { Markdown } from '../components/Markdown';
+import AllTokens from '../images/All tokens.png';
+import NegativeWordCloud from '../images/Negative tweets.png';
+import PositiveWordCloud from '../images/Positive tweets.png';
+import '../styles/Slick.scss';
 import { Section } from './Section';
+import * as styles from './Sentiment.module.scss';
 
 interface ISentiment {
     data?: number[];
@@ -25,6 +31,9 @@ export class Sentiment extends React.Component<{}, ISentiment> {
         return (
             <section id={Section.Sentiment}>
                 <Markdown filename={'sentiment.md'} />
+
+                {this.renderSlick()}
+
                 {this.state.data ? (
                     <Histogram
                         data={this.state.data}
@@ -32,6 +41,32 @@ export class Sentiment extends React.Component<{}, ISentiment> {
                     />
                 ) : null}
             </section>
+        );
+    }
+
+    private renderSlick() {
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        };
+
+        return (
+            <div className={styles.sliderContainer}>
+                <Slider {...settings}>
+                    <div>
+                        <img src={AllTokens} />
+                    </div>
+                    <div>
+                        <img src={PositiveWordCloud} />
+                    </div>
+                    <div>
+                        <img src={NegativeWordCloud} />
+                    </div>
+                </Slider>
+            </div>
         );
     }
 }
